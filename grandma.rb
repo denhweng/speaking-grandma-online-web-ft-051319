2bb93dafb8706bb64216367fb9bd14cdddba1fa1
Dan Hwang <danhwangfkpc@gmail.com>
2019-05-15 21:24:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase  
+  elsif phrase !== phrase.upcase  
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase == true 
+  elsif phrase == phrase.upcase 
     puts "NO, NOT SINCE 1938!"
   end 
   
